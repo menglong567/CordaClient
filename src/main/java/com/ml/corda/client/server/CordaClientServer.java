419254e9e8dbcf286331c0ea2dc40a0277d67e3f
leo <menglong567@aliyun.com>
2020-04-05 16:08:50 +0800

    enable swagger / build-plugin / gson

--- src/main/java/com/ml/corda/client/server/CordaClientServer.java
+++ src/main/java/com/ml/corda/client/server/CordaClientServer.java
@@ -3,11 +3,13 @@ package com.ml.corda.client.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author mengl
  */
 @SpringBootApplication(scanBasePackages = "com.ml")
+@EnableSwagger2
 public class CordaClientServer {
     public static void main(String[] args) {
         SpringApplication.run(CordaClientServer.class, args);
